10f14a00ed3f44a04d76b5b234764db5c3bedf04
husan999 <husanshunqorov07@gmail.com>
2023-02-03 21:34:38 -0700

    good

--- oldest_person.py
+++ oldest_person.py
@@ -6,4 +6,13 @@ def oldest(people:dict):
     Returns:
         str: the name of the oldest person
     """
-    return
+    a=0
+    b=0
+    for i in people:
+        x=people[i]
+        if x>a:
+            a=x
+            b=i
+    return b
+    
+print(oldest({"Javohir": 22, "Sharof": 23, "Tolib": 34, "Rustam": 16}))
